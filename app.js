52118c6aeb57331b00589c8aef42265d1d6678ca
miduki-yano <72035301+miduki-yano@users.noreply.github.com>
2024-02-04 23:52:54 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require('ejs');
 const url = require('url');
 const qs = require('querystring');
 
+
 const index_page = fs.readFileSync('./index.ejs','utf8');
 const other_page = fs.readFileSync('./other.ejs','utf8');
 const style_css = fs.readFileSync('./style.css','utf8');
@@ -13,6 +14,28 @@ var server = http.createServer(getFromClient);
 server.listen(3000);
 console.log('Server start!');
 
+function setCookie(key,value,response){
+        var cookie = qs.escape(value);
+        response.setHeader('Set-Cookie',[key + '=' + cookie]);
+}
+
+function getCookie(key,request){
+        var cookie_data = request.headers.cookie != undefined ?
+        request.headers.cookie: '';
+        //条件？値１：値２；
+        //条件に合えば、値１が入り、合っていないなら値２が入る。
+        console.log(cookie_data);
+        var data = cookie_data.split(';');
+        
+        for (var i in data){
+                if(data[i].trim().startsWith(key + '=')){
+                        var result = data[i].trim().substring(key.length + 1);
+                        return qs.unescape(result);
+                }
+        }
+        return '';
+}
+
 
 
 function getFromClient(request,response){
@@ -20,29 +43,10 @@ function getFromClient(request,response){
         switch(url_parts.pathname){
 
                 case '/':
-                        /*
-                        var content = "これはIndexページです"
-                        var query = url_parts.query;
-                        if(query.msg != undefined) {
-                                content += 'あなたは、「' + query.msg + '」と送りました。';
-                        }
-                        var content = ejs.render(index_page,{
-                                title: "Indexページ",
-                                content: content,
-                        });
-                        */
-
                         response_index(request,response);
                         break;
 
                 case '/other':
-                        /*var content = ejs.render(other_page,{
-                                title: "other",
-                                content: "これは新しいページ",
-                        });
-                        response.writeHead(200,{'Content-Type':'text/html'});
-                        response.write(content);
-                        response.end();*/
                         response_other(request,response);
                         break;
                 case '/style.css':
@@ -59,13 +63,8 @@ function getFromClient(request,response){
         } 
 }
 
-/*
-var data = {
-        'Taro': '09-999-999',
-        'Hanako': '080-888-888',
-        'Sachiko': '070-777-777',
-        'Ichiro': '060-666-666'
-};*/
+
+
 
 var data = { msg: 'no message...'};
 
@@ -78,36 +77,29 @@ function response_index(request,response){
                 });
                 request.on('end',() =>{
                         data = qs.parse(body);
+                        setCookie('msg',data.msg,response);
                         write_index(request,response);
                 });
         }else{
                 write_index(request,response);
         }
-        
-        /*
-        var msg = "これはindexページです。"
-        var content = ejs.render(index_page,{
-                title: "Index",
-                content: msg,
-                data: data,
-                filename: 'data_item',
-        });
-        response.writeHead(200,{'Content-Type':'text/html'});
-                response.write(content);
-                response.end();*/
+
 }
 
 function write_index(request,response){
         var msg = "※伝言を表示します。"
+        var cookie_data = getCookie('msg',request);
         var content = ejs.render(index_page,{
                 title: "Index",
                 content: msg,
                 data: data,
+                cookie_data: cookie_data,
         });
         response.writeHead(200,{ 'Content-Type': 'text/html'});
         response.write(content);
         response.end();
 }
+
 
 var data2 = {
         'Taro': ['taro@yamada','09-999-999','Tokyo'],
@@ -117,6 +109,7 @@ var data2 = {
 }
 
 //otherのアクセス処理
+
 function response_other(request,response){
         var msg = "これはotherページです。"
         var content = ejs.render(other_page,{
@@ -129,31 +122,4 @@ function response_other(request,response){
                 response.write(content);
                 response.end();
 }
-        /*
-        if(request.method == 'POST'){
-                var body = '';
-                request.on('data', (data) => {
-                        body += data;
-                });
-                request.on('end',() =>{
-                        var post_data = qs.parse(body);
-                        msg += 'あなたは、「' + post_data.msg + '」と書きました。';
-                        var content = ejs.render(other_page, {
-                                title: "Other",
-                                content: msg,
-                        });
-                        response.writeHead(200,{'Content-Type':'text/html'});
-                        response.write(content);
-                        response.end();
-
-                });
-        }else{
-                var msg = "メッセージがありません。"
-                var content = ejs.render(other_page, {
-                        title: "Other",
-                        content: msg,
-                });
-                response.writeHead(200,{'Content-Type':'text/html'});
-                response.write(content);
-                response.end();
-        }*/
+        
